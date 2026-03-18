679a0722b4ed97dd93fb8ef1db555fd927605cee
nkl341 <150189439+nkl341@users.noreply.github.com>
2026-03-18 13:15:59 -0500

    Altered For Horizontal ULA

--- range-finding/control_servo_test.m
+++ range-finding/control_servo_test.m
@@ -7,23 +7,13 @@ r = raspi('169.254.52.8','analog','analog');
     if ~exist('s_el', 'var')
     s_el = servo(r, 12, 'MinPulseDuration', 5.44e-4, 'MaxPulseDuration', 2.40e-3);  
     end
-    dev = serialdev(r, '/dev/serial0', 115200, 8, 'none', 1);
-    dev.Timeout = 0.05;
+    % dev = serialdev(r, '/dev/serial0', 115200, 8, 'none', 1);
+    % dev.Timeout = 0.05;
 
 
 % 4) Sweep example
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-for ang = 0:5:160
-    get_range_v1(r,ang,170,dev,s_az,s_el)
-=======
-for ang = 0:20:360
-    get_range_v4(r,ang,170,s_az,s_el,10);
->>>>>>> Stashed changes
-=======
-for ang = 0:20:360
-    get_range_v4(r,ang,170,s_az,s_el,10);
->>>>>>> Stashed changes
+for ang = -90:5:180
+    get_range_v3_5(r,ang,170,s_az,s_el)
 end
 
 % get_range_v0(r,50,120);
